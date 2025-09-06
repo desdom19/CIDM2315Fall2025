f6e8e44db98bff217d13688c6b330b03dbab9640
desdom19 <ddominguez2@buffs.wtamu.edu>
2025-09-06 17:36:05 -0500

    Add files via upload

---
+++ Homework2 Task1.cs
@@ -0,0 +1,37 @@
+﻿namespace Homework2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input a letter grade: ");
+        string letter = Console.ReadLine();
+
+        
+        if (letter == "A")
+        {
+            Console.WriteLine("GPA Point: 4");
+        }
+        else if (letter == "B")
+        {
+            Console.WriteLine("GPA Point: 3");
+        }
+        else if (letter == "C")
+        {
+            Console.WriteLine("GPA Point: 2");
+        }
+        else if (letter == "D")
+        {
+            Console.WriteLine("GPA Point: 1");
+        }
+        else if (letter == "F")
+        {
+            Console.WriteLine("GPA Point: 0");
+        }
+        else
+        {
+            Console.WriteLine("Wrong Letter Grade!");
+        }
+
+    }
+}
