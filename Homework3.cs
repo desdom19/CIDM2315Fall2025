56c642c6bb0f445322b1f8c0c78ecf3e030c4b75
desdom19 <ddominguez2@buffs.wtamu.edu>
2025-09-15 13:50:22 -0500

    Add files via upload

---
+++ Homework3.cs
@@ -0,0 +1,70 @@
+﻿namespace Homework3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        // Q1 Code
+        Console.WriteLine("Input an integer: ");
+        int n = Convert.ToInt16(Console.ReadLine());
+
+        if (n == 1)
+        {
+            Console.WriteLine("N is not prime.");
+        }
+        else
+        {
+            int i = 2;
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("N is non-prime.");
+                    break;
+                }
+                i++;
+            }
+
+            if (i == n)
+            {
+                Console.WriteLine("N is prime.");
+
+            }
+
+        }
+
+
+       // Q2 Code
+            Console.WriteLine("Assign an int value to N: ");
+        int N = Convert.ToInt16(Console.ReadLine());
+
+        for (int row = 0; row < N; row++)
+        {
+            for (int col = 0; col < N; col++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine("");
+        }
+         
+
+        // Q3 Code
+            Console.WriteLine("Assign an int value to N: ");
+         int N = Convert.ToInt16(Console.ReadLine());
+
+         for (int row = 0; row < N; row++) {
+             for (int col = 0; col < N; col++)
+             {
+                 if (row >= col)
+                     Console.Write("*");
+             }
+             Console.WriteLine("");
+         }
+
+         */
+    }
+}
+
+    
+
