f6d5966d7fea50092d8ae9e86a89ccbf0d6c71f4
desdom19 <ddominguez2@buffs.wtamu.edu>
2025-09-22 00:04:12 -0500

    Add files via upload

---
+++ Homework4.cs
@@ -0,0 +1,124 @@
+﻿namespace Homework4;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+/*
+        // Call Q1_method()
+
+        Console.WriteLine("Enter first number: ");
+        string first_num = Console.ReadLine();
+        int first_int = Convert.ToInt16(first_num);
+
+        Console.WriteLine("Enter second number: ");
+        string second_num = Console.ReadLine();
+        int second_int = Convert.ToInt16(second_num);
+
+        Console.WriteLine($"a = {first_int}; b = {second_int}");
+
+        int returned_largest = LargestNumber(first_int, second_int);
+        Console.WriteLine($"The largest number is: {returned_largest}");
+        
+
+
+       // Call Q2_method()
+
+       
+    Console.WriteLine("Enter a number: ");
+        string num = Console.ReadLine();
+        int num_int = Convert.ToInt16(num);
+
+
+        Console.WriteLine("Enter left or right: ");
+        string which_shape = Console.ReadLine();
+
+        PrintShape(num_int, which_shape);
+
+
+    }
+
+        // Q1_method
+    static int LargestNumber(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+
+
+        }
+        
+
+        // Q2_method
+
+
+        static void PrintShape(int N, string shape)
+                {
+                    if (shape == "left")
+                    {
+                        for (int row = 0; row < N; row++)
+                        {
+                            for (int col = 0; col < N; col++)
+                            {
+                                if (row >= col)
+                                    Console.Write("*");
+                            }
+                            Console.WriteLine("");
+                        }
+                    }
+                    else if (shape == "right")
+                    {
+                        for (int row = 0; row <= N - 1; row++)
+                        {
+                            for (int col = 0; col <= N - 2; col++)
+                            {
+                                if (row <= col)
+                                    Console.Write(" ");
+
+                            }
+
+                            for (int col = 0; col < N; col++)
+                            {
+                                if (row >= col)
+                                    Console.Write("*");
+
+
+                            }
+
+
+                            Console.WriteLine("");
+
+                        }
+
+
+                    } */
+
+        }
+
+    }
+
+    
+    
+
+
+        
+
+
+
+
+
+            
+            
+
+            
+        
+
+
+
+    
